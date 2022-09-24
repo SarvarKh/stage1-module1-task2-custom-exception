104f533adc518c32d703b33515fc877fddfd81e4
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-24 11:47:00 +0500

    Solve Task #02 of Stage 1

---
+++ src/main/java/com/epam/mjc/StudentException.java
@@ -0,0 +1,7 @@
+package com.epam.mjc;
+
+public class StudentException extends IllegalArgumentException {
+    public StudentException(String s) {
+        super(s);
+    }
+}
--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,16 +5,24 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentException {
+    if (Student.getValueOf(studentID) == null) {
+      throw new StudentException("Could not find student with ID " + studentID);
+    } else {
+      return Student.getValueOf(studentID);
+    }
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+
+      try {
+        Student student = manager.find(IDs[i]);
+      } catch (StudentException ex) {
+        System.out.println(ex);
+      }
     }
 
   }
